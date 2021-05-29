69b37738bc3b4a87c89632d1ba81a1b3229280c4
naoto-mizunuma <n-mizunuma@froide.co.jp>
2021-05-29 20:39:54 +0900

    コレクション管理用のページ追加

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+const path = require("path");
 const vm = require('vm');
-const fs = require('fs');
+const fs = require('fs-extra');
 const http = require('http');
+const mime = require('mime-types');
 const ws = require('ws');
 const net = require('net');
 const repl = require('repl');
 const { processTopLevelAwait } = require("node-repl-await");
+
+
+const stat = async _path=>await fs.stat(_path).catch(()=>null);
 
 (async function(config){
   const Node = await require('./Node.js');
@@ -32,9 +37,24 @@ const { processTopLevelAwait } = require("node-repl-await");
     repl_server.context.Node = Node;
   }).listen(config.repl.port, config.repl.server);
 
-
   // HTTP, WebSocket Server
-  const server = http.createServer(function(req, res){});
+  const server = http.createServer(async function(req, res){
+    const _path = `.${req.url}`;
+
+    if(await stat(_path)){
+      const mime_type = mime.contentType(path.extname(_path));
+      res.writeHead(200, {'content-type': mime_type});
+
+      let data = Buffer.from([]);
+      const value_path = path.join(_path, ".value");
+      if(await stat(value_path)) data = await fs.readFile(value_path);
+      res.write(data);
+    }else{
+      res.writeHead(404);
+      res.write("404 not found!");
+    }
+    res.end();
+  });
   new ws.Server({server: server}).on('connection', function(wso, req){
     let set_result = async function(m){ wso.send(`${m} 404not found!`); };
     let set_close = async function(m){ wso.send(`${m} connection closed!`); };
@@ -51,10 +71,11 @@ const { processTopLevelAwait } = require("node-repl-await");
     wso.on('close', async function(){ await set_close(); });
   });
   server.listen(config.http.port, function(){
-    console.log(`run server localhost:${config.http.port}/[echo,repl]`);
+    console.log(`run server!`);
+    console.log(`  http://localhost:${config.http.port}/index.html`);
+    console.log(`  ws://localhost:${config.repl.port}/[echo,repl]`);
   });
 })({
   http: {port: 5001},
   repl: {port: 5002, server: 'localhost', prefix: '> '}
 });
-
